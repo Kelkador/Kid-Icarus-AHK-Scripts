235f42c08af12b3e3e1ce9259e338b1a8cd5ddf9
Kelkador <kelkador@gmail.com>
2023-09-20 13:25:52 -0500

    Adding references to evilC's work

---
+++ C#/ViGEmWrapper/TestApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Nefarius.ViGEm.Client.Targets.DualShock4;
+using Nefarius.ViGEm.Client.Targets.Xbox360;
+using ViGEmWrapper;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var xb = new Xb360();
+            //xb.SetButtonState(Xbox360Buttons.A, true);
+            //xb.SendReport();
+            //xb.SetButtonState(Xbox360Buttons.A, false);
+            //xb.SendReport();
+            //xb.SubscribeFeedback(new Action<byte, byte, byte>(OnXbFeedback));
+
+            var ds4 = new Ds4();
+            ds4.SetButtonState(DualShock4Buttons.Circle, true);
+            ds4.SendReport();
+            ds4.SubscribeFeedback(new Action<byte, byte, string>(OnDs4Feedback));
+
+            ds4.SetButtonState(DualShock4Buttons.Circle, false);
+            ds4.SendReport();
+            Console.ReadLine();
+        }
+
+        private static void OnXbFeedback(byte largeMotor, byte smallMotor, byte ledNumber)
+        {
+            Console.WriteLine($"XB360 Feedback received - LargeMotor: {largeMotor}, SmallMotor: {smallMotor}. LedNumber: {ledNumber}");
+        }
+
+        private static void OnDs4Feedback(byte largeMotor, byte smallMotor, string lightbarColor)
+        {
+            Console.WriteLine($"DS4 Feedback received - LargeMotor: {largeMotor}, SmallMotor: {smallMotor}. LightbarColor: {lightbarColor}");
+        }
+    }
+}
---
+++ C#/ViGEmWrapper/ViGEmWrapper/Xb360.cs
@@ -0,0 +1,51 @@
+﻿using Nefarius.ViGEm.Client.Targets;
+using Nefarius.ViGEm.Client.Targets.Xbox360;
+
+namespace ViGEmWrapper
+{
+    public class Xb360
+    {
+        private readonly Xbox360Controller _controller;
+        private readonly Xbox360Report _report;
+        private dynamic _feedbackCallback;
+
+        public Xb360()
+        {
+            _controller = new Xbox360Controller(Client.ViGEmClient);
+            _report = new Xbox360Report();
+            _controller.Connect();
+        }
+
+        public string OkCheck()
+        {
+            return "OK";
+        }
+
+        public void SetButtonState(Xbox360Buttons btn, bool state)
+        {
+            _report.SetButtonState(btn, state);
+        }
+
+        public void SetAxisState(ushort axis, short state)
+        {
+            _report.SetAxis((Xbox360Axes)axis, state);
+        }
+
+        public void SendReport()
+        {
+            _controller.SendReport(_report);
+        }
+
+        public void SubscribeFeedback(dynamic callback)
+        {
+            _feedbackCallback = callback;
+            _controller.FeedbackReceived += OnFeedbackReceived;
+        }
+
+        private void OnFeedbackReceived(object sender, Xbox360FeedbackReceivedEventArgs e)
+        {
+            if (_feedbackCallback == null) return;
+            _feedbackCallback(e.LargeMotor, e.SmallMotor, e.LedNumber);
+        }
+    }
+}
